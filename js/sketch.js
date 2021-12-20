0333674f3e30e5eeb34c8ff4a1b3062ae889cf77
Elias Wolf <92587199+EliasWolf-Br@users.noreply.github.com>
2021-12-19 21:19:07 -0300

    comments

--- js/sketch.js
+++ js/sketch.js
@@ -1,59 +1,64 @@
 let game;
 
 function setup() {
+	// instance class
 	game = new Game(20, 20, 30);
 }
 
 function draw() {
+	// update and draw game
 	game.update();
 
 	game.draw();
 }
 
 function keyPressed(e) {
+	// reset on Z pressed
 	if(e.keyCode == 90) setup();
 }
 
 function windowResized() {
+	// center canvas horizontal
 	game.canvas.center("horizontal");
 }
 
 class Game {
 	constructor(w, h, s) {
-		this.size = s;
-		this.width = w;
-		this.height = h;
+		this.size = s; // size of cells
+		this.width = w; // width board
+		this.height = h; // height board
 
-		this.snake = {
+		this.snake = { // snake
 			body: [],
 			dir: -1,
 			died: false
 		};
 
-		for(var a = 0; a < 2; a++) this.snake.body.push(createVector(1, 1));
+		for(var a = 0; a < 2; a++) this.snake.body.push(createVector(1, 1)); // add body to snake
 
-		this.food = createVector(randomInt(0, this.width), randomInt(0, this.height));
+		this.food = createVector(randomInt(0, this.width), randomInt(0, this.height)); // food
 
-		this.tick = 0;
+		this.tick = 0; // runtime
 
-		this.canvas = createCanvas(this.width * this.size, this.height * this.size).center("horizontal");
+		this.canvas = createCanvas(this.width * this.size, this.height * this.size).center("horizontal"); // canvas
 	}
 
 	update() {
+		// change snake dir
 		if(keyIsDown(37) && this.snake.dir != 1) this.snake.dir = 0;
 		if(keyIsDown(39) && this.snake.dir != 0) this.snake.dir = 1;
 		if(keyIsDown(38) && this.snake.dir != 3) this.snake.dir = 2;
 		if(keyIsDown(40) && this.snake.dir != 2) this.snake.dir = 3;
 
-		this.tick++;
+		this.tick++; // move runtime
 
-		if(this.tick % 4 != 3 || this.snake.died) return;
+		if(this.tick % 4 != 3 || this.snake.died) return; // return if died
 
-		var newSnake = this.snake.body[0].copy();
+		var newSnake = this.snake.body[0].copy(); // copy snake position
 
 		if(this.snake.dir < 0) return;
 
-		switch(this.snake.dir) {
+		switch(this.snake.dir) { // move position
 			case 0:
 				newSnake.x -= 1;
 				break;
@@ -68,11 +73,13 @@ class Game {
 				break;
 		}
 
+		// warp snake
 		if(newSnake.x < 0) newSnake.x = (this.width - 1);
 		if(newSnake.x > (this.width - 1)) newSnake.x = 0;
 		if(newSnake.y < 0) newSnake.y = (this.height - 1);
 		if(newSnake.y > (this.height - 1)) newSnake.y = 0;
 
+		// check snake self-collision
 		for(var a = 1; a < this.snake.body.length; a++) {
 			var snake = this.snake.body[a];
 			if(snake != undefined) {
@@ -82,10 +89,12 @@ class Game {
 				}
 			}
 		}
-
+		
+		// move snake
 		this.snake.body.pop();
 
 		if(this.food.x == newSnake.x && this.food.y == newSnake.y) {
+			// eat food?
 			this.snake.body.unshift(newSnake);
 			this.food = createVector(randomInt(0, this.width), randomInt(0, this.height));
 		}
@@ -94,30 +103,21 @@ class Game {
 	}
 
 	draw() {
+		// draw game
 		background(255);
 
 		noStroke();
 
-		var border = 4;
+		// cool effect
+		var border = 4; fill(0); rect((this.food.x * this.size) - border, (this.food.y * this.size) - border, this.size + (border * 2), this.size + (border * 2)); for(var a = 0; a < this.snake.body.length; a++) rect((this.snake.body[a].x * this.size) - border, (this.snake.body[a].y * this.size) - border, this.size + (border * 2), this.size + (border * 2));
 
-		fill(0);
-		rect(
-			(this.food.x * this.size) - border, (this.food.y * this.size) - border,
-			this.size + (border * 2), this.size + (border * 2)
-		);
-
-		fill(0);
-		for(var a = 0; a < this.snake.body.length; a++) {
-			rect(
-				(this.snake.body[a].x * this.size) - border, (this.snake.body[a].y * this.size) - border,
-				this.size + (border * 2), this.size + (border * 2)
-			);
-		};
-
+		// draw food
 		fill(255, 255, 0);
 		rect(this.food.x * this.size, this.food.y * this.size, this.size, this.size);
-
+		
+		// draw snake
 		for(var a = 0; a < this.snake.body.length; a++) {
+			// gradient effect
 			var gradient = (a / (this.snake.body.length - 1)) * 255 || 0;
 			fill(gradient, 0, 255 - gradient);
 			rect(this.snake.body[a].x * this.size, this.snake.body[a].y * this.size, this.size, this.size);
